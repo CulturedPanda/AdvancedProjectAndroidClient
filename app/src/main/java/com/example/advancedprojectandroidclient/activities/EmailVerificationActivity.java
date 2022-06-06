27883b1961debc1f4c7a101171c23b8b2ac4b820
nadav elgrabli <nadav.elgrabli@live.biu.ac.il>
2022-06-06 17:27:04 +0300

    started working on email veification layout and chat layout + message bubbles

---
+++ app/src/main/java/com/example/advancedprojectandroidclient/activities/EmailVerificationActivity.java
@@ -0,0 +1,16 @@
+package com.example.advancedprojectandroidclient.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.advancedprojectandroidclient.R;
+
+public class EmailVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_verification);
+    }
+}
--- app/src/main/java/com/example/advancedprojectandroidclient/activities/SignUpActivity.java
+++ app/src/main/java/com/example/advancedprojectandroidclient/activities/SignUpActivity.java
@@ -3,14 +3,30 @@ package com.example.advancedprojectandroidclient.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.advancedprojectandroidclient.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    //get the spinner of security questions from the xml.
+    Spinner questionsList = findViewById(R.id.securitySpinner);
+    //create a list of questions for the spinner.
+    String[] items = new String[]{"What was your elementary's school name?",
+            "What is / was your dog's name?",
+            "What is your favorite sport?",
+            "In what city did you grow up in?",
+            "What was your nick-name as a child?",
+            "What was your favorite teacher's name?"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        questionsList.setAdapter(adapter);
     }
 }
