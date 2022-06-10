0d4e640692d5aca649733fdf92bde37fd16b873e
nadav elgrabli <nadav.elgrabli@live.biu.ac.il>
2022-06-10 12:58:39 +0300

    added forgot pass and reset pass activities and layouts

---
+++ app/src/main/java/com/example/advancedprojectandroidclient/activities/ForgotPasswordActivity.java
@@ -0,0 +1,17 @@
+package com.example.advancedprojectandroidclient.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.advancedprojectandroidclient.R;
+
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/example/advancedprojectandroidclient/activities/ResetPasswordActivity.java
@@ -0,0 +1,17 @@
+package com.example.advancedprojectandroidclient.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.advancedprojectandroidclient.R;
+
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
