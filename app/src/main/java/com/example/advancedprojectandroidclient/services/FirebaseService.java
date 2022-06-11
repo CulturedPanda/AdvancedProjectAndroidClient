5d5c2033a97401cffa0137f748aa25b1da366254
Yuval <yuvaluner@gmail.com>
2022-06-11 13:20:13 +0300

    Finished adding notification via firebase

--- app/src/main/java/com/example/advancedprojectandroidclient/services/FirebaseService.java
+++ app/src/main/java/com/example/advancedprojectandroidclient/services/FirebaseService.java
@@ -1,13 +1,18 @@
 package com.example.advancedprojectandroidclient.services;
 
 import android.app.NotificationChannel;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.example.advancedprojectandroidclient.MyApplication;
 import com.example.advancedprojectandroidclient.R;
+import com.example.advancedprojectandroidclient.activities.ChatActivity;
 import com.example.advancedprojectandroidclient.api.RegisteredUserApi;
 import com.google.firebase.messaging.FirebaseMessagingService;
 
@@ -16,6 +21,7 @@ public class FirebaseService extends FirebaseMessagingService {
     public static String token;
 
     public FirebaseService() {
+        MyApplication.refreshTokenRepository.autoRenewTokens(1, 100);
     }
 
     public static void sendRegistrationToServer(String token) {
@@ -30,18 +36,35 @@ public class FirebaseService extends FirebaseMessagingService {
         sendRegistrationToServer(token);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onMessageReceived(@NonNull com.google.firebase.messaging.RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        if (remoteMessage.getNotification() != null){
+        String username = remoteMessage.getData().get("username");
+        String current = MyApplication.messagesRepository.getWith();
+        MyApplication.messagesRepository.setWith(remoteMessage.getData().get("username"));
+        MyApplication.messagesRepository.getAll();
+        // Second condition is to prevent the notification from being sent when the user is in the chat activity
+        // With the user who sent the message
+        if (remoteMessage.getNotification() != null &&
+                (!ChatActivity.running || !current.equals(username) )){
             createNotificationChannel();
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+            Intent i = new Intent(this, ChatActivity.class);
+            i.putExtra("contactName", remoteMessage.getData().get("nickname"));
+            i.putExtra("contactId", remoteMessage.getData().get("username"));
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_IMMUTABLE);
 
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.channel_id))
                     .setSmallIcon(R.drawable.ic_launcher_background)
                     .setContentTitle(remoteMessage.getNotification().getTitle())
                     .setContentText(remoteMessage.getNotification().getBody())
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                    .setStyle(new NotificationCompat.BigTextStyle()
+                            .bigText(remoteMessage.getNotification().getBody()))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent);
 
             notificationManager.notify(String.valueOf(remoteMessage.getNotification().getTitle()).hashCode(), builder.build());
         }
